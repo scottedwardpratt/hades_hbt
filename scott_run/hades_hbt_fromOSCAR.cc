f4343a9f4185c214b80b7910b741cb6f3498cbae
scottedwardpratt <prattsc@msu.edu>
2022-10-12 10:18:50 -0400

    dummy message

---
+++ scott_run/hades_hbt_fromOSCAR.cc
@@ -0,0 +1,17 @@
+#include "msu_commonutils/commonutils.h"
+#include "msu_coral/coral.h"
+#include "hades_hbt/hades_hbt.h"
+
+using namespace std;
+
+int main(){
+	string parsfilename="parameters/coralpars.dat";
+	Chades_hbt_master *hades_hbt_master=new Chades_hbt_master(parsfilename);
+	hades_hbt_master->ReadOSCAR();
+	hades_hbt_master->CalcCFs();
+	hades_hbt_master->cfs->PrintC_of_qinv();
+	hades_hbt_master->cfs->WriteC_of_qinv("qinv_output/qinv.txt");
+	hades_hbt_master->cfs->WriteC3D("threed_output");
+	return 0;
+}
+
